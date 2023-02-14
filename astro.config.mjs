c135cb6c988638350d8a0e85617ff2c2a760912a
Santiago <santiagocortes996@hotmail.com>
2023-02-14 08:24:04 -0300

    Astro Landing Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: "https://Santaas96.github.io",
+  base: "/astro-tailwind-landing",
 });
